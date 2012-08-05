d7c2c80a484a55a38812e70e39f6b863f37bc0e9
Tony Jian <tonytonyjan@gmail.com>
2012-08-05 14:27:39 +0800

    update

--- js/Course-History-adList.js
+++ js/Course-History-adList.js
@@ -1,6 +1,6 @@
 $('td:first-child a').each(function(){
   var url = this.href;
   var options = {url: url, dataLayout: "button_count"};
-  var btn = $(FBLikeBtn(options));
+  var btn = $("</br>" + FBLikeBtn(options));
   $(this).closest('td').append(btn);
 });
